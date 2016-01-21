c426d84d26fbdde2fc67feba3ea401c97d6b351e
Nhan Cao <caovannhan2002@gmail.com>
2016-01-21 15:37:34 +0530

    remove default menu

--- app/src/main/java/org/hispindia/bidtrackerreports/ui/activity/HIActivityMain.java
+++ app/src/main/java/org/hispindia/bidtrackerreports/ui/activity/HIActivityMain.java
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.hisp.dhis.android.sdk.controllers.DhisService;
 import org.hisp.dhis.android.sdk.controllers.LoadingController;
@@ -67,23 +65,6 @@ public class HIActivityMain extends AppCompatActivity implements INavigationHand
         super.onPause();
         Dhis2Application.getEventBus().unregister(this);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
