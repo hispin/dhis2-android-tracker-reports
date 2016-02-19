9c76ae56fc18d5108b756e07982b030fd5b85f67
Sourabh Bhardwaj <bsourabh191@gmail.com>
2016-02-19 15:26:14 +0530

    String added

---
+++ app/src/main/java/org/hispindia/bidtrackerreports/ui/fragment/histockreport/histockinhandvsdemandfordistrict/HIFragmentStockInHandvsDemandforDistrictReport.java
@@ -0,0 +1,11 @@
+package org.hispindia.bidtrackerreports.ui.fragment.histockreport.histockinhandvsdemandfordistrict;
+
+/**
+ * Created by Sourabh on 2/18/2016.
+ */
+public class HIFragmentStockInHandvsDemandforDistrictReport  {
+
+
+
+
+}
